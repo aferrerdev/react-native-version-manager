1801ae60405494477ce218c55c3265452a9c5e4f
Alex Ferrer Lopez <alex.ferrer@manomano.com>
2024-11-21 17:15:58 +0100

    Improve Android implementation

--- android/src/main/java/com/versionmanager/VersionManagerModule.kt
+++ android/src/main/java/com/versionmanager/VersionManagerModule.kt
@@ -8,6 +8,7 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableArray
+import java.util.HashSet
 
 class VersionManagerModule internal constructor(context: ReactApplicationContext) :
   VersionManagerSpec(context) {
@@ -37,17 +38,16 @@ class VersionManagerModule internal constructor(context: ReactApplicationContext
           val packageInfo = packageManager.getPackageInfo(context.packageName, 0)
           val currentVersion = packageInfo.versionName
 
-          // Convert ReadableArray to List<String>
-          val deprecatedList = mutableListOf<String>()
+          // Use HashSet for faster lookup
+          val deprecatedSet = HashSet<String>()
           for (i in 0 until deprecatedVersions.size()) {
-              val version = deprecatedVersions.getString(i)
-              if (version != null) {
-                  deprecatedList.add(version)
+              deprecatedVersions.getString(i)?.let { version ->
+                  deprecatedSet.add(version)
               }
           }
 
-          // Check if the current version is in the deprecated list
-          val isDeprecated = deprecatedList.contains(currentVersion)
+          // Check if the current version is in the deprecated set
+          val isDeprecated = deprecatedSet.contains(currentVersion)
           promise.resolve(isDeprecated)
       } catch (e: Exception) {
           promise.reject("ERROR", "Failed to determine if app version is deprecated", e)
