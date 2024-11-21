65ea456ef71e9b0fc999891baef2004c496f0193
Alex Ferrer Lopez <alex.ferrer@manomano.com>
2024-11-21 17:13:04 +0100

    Add Android changes and update .gitignore

--- android/src/main/java/com/versionmanager/VersionManagerModule.kt
+++ android/src/main/java/com/versionmanager/VersionManagerModule.kt
@@ -1,21 +1,57 @@
 package com.versionmanager
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactMethod
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableArray
 
 class VersionManagerModule internal constructor(context: ReactApplicationContext) :
   VersionManagerSpec(context) {
 
   override fun getName(): String {
-    return NAME
+    return NAME;
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  override fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  override fun getAppVersion(promise: Promise) {
+      try {
+          val context = reactApplicationContext
+          val packageManager = context.packageManager
+          val packageInfo = packageManager.getPackageInfo(context.packageName, 0)
+          val version = packageInfo.versionName
+          promise.resolve(version)
+      } catch (e: Exception) {
+          promise.reject("ERROR", "Failed to get app version", e)
+      }
+  }
+
+  @ReactMethod
+  override fun isAppVersionDeprecated(deprecatedVersions: ReadableArray, promise: Promise) {
+      try {
+          val context = reactApplicationContext
+          val packageManager = context.packageManager
+          val packageInfo = packageManager.getPackageInfo(context.packageName, 0)
+          val currentVersion = packageInfo.versionName
+
+          // Convert ReadableArray to List<String>
+          val deprecatedList = mutableListOf<String>()
+          for (i in 0 until deprecatedVersions.size()) {
+              val version = deprecatedVersions.getString(i)
+              if (version != null) {
+                  deprecatedList.add(version)
+              }
+          }
+
+          // Check if the current version is in the deprecated list
+          val isDeprecated = deprecatedList.contains(currentVersion)
+          promise.resolve(isDeprecated)
+      } catch (e: Exception) {
+          promise.reject("ERROR", "Failed to determine if app version is deprecated", e)
+      }
   }
 
   companion object {
--- android/src/oldarch/VersionManagerSpec.kt
+++ android/src/oldarch/VersionManagerSpec.kt
@@ -1,11 +1,14 @@
 package com.versionmanager
 
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReadableArray
 
 abstract class VersionManagerSpec internal constructor(context: ReactApplicationContext) :
   ReactContextBaseJavaModule(context) {
 
-  abstract fun multiply(a: Double, b: Double, promise: Promise)
+  abstract fun getAppVersion(promise: Promise)
+
+  abstract fun isAppVersionDeprecated(deprecatedVersions: ReadableArray, promise: Promise)
 }
